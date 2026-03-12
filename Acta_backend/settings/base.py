985b9a30acc2d77297cf785483d5d74bd93ec435
oluwaseyipd <oluwaseyipd@gmail.com>
2026-03-12 19:26:31 +0100

    fix: update storage config

--- Acta_backend/settings/base.py
+++ Acta_backend/settings/base.py
@@ -102,7 +102,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
