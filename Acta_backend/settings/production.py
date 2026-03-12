df1f2b80a3e1ad2e9bf02b16a81c310d0cbe9e9c
oluwaseyipd <oluwaseyipd@gmail.com>
2026-03-12 20:32:10 +0100

    fix (deploy): update media url in production.py

--- Acta_backend/settings/production.py
+++ Acta_backend/settings/production.py
@@ -99,6 +99,8 @@ STORAGES = {
     },
 }
 
+MEDIA_URL = '/media/'
+
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
